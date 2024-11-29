4fa457ad7ec81c7e584effc62c9bf13e5bb57a12
Brainless-Loco <dev.tcdas@gmail.com>
2024-11-30 02:15:01 +0600

    Navigation Icons are visible now

--- screens/Dashboard/Dashboard.js
+++ screens/Dashboard/Dashboard.js
@@ -4,30 +4,47 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../../components/Dashboard/Tabs/Home/Home';
 import Activities from '../../components/Dashboard/Tabs/Activities/Activities';
 import AboutUs from '../../components/Dashboard/Tabs/AboutUs/AboutUs';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import FontAwesome  from '@expo/vector-icons/FontAwesome';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function Dashboard({ route, navigation }) {
 
+  const focusedIcons = [
+    <Ionicons name="home-outline" size={24} color="black" />,
+    <Ionicons name="newspaper-outline" size={24} color="black" />,
+    <AntDesign name="infocirlceo" size={24} color="black" />
+  ]
+
+  const unFocusedIcons = [
+    <Ionicons name="home-sharp" size={24} color="#0044ba" />,
+    <Ionicons name="newspaper-sharp" size={24} color="#0044ba" />,
+    <AntDesign name="infocirlce" size={24} color="#0044ba" />
+  ]
+
   const tabComponents = [
     {
-      name: "Home", component: Home
+      name: "Home", component: Home,
+      focusedIcon: focusedIcons[0], unFocusedIcon: unFocusedIcons[0]
     },
     {
-      name: "Activities", component: Activities
+      name: "Activities", component: Activities,
+      focusedIcon: focusedIcons[1], unFocusedIcon: unFocusedIcons[1]
     },
     {
-      name: "About Us", component: AboutUs
+      name: "About Us", component: AboutUs,
+      focusedIcon: focusedIcons[2], unFocusedIcon: unFocusedIcons[2]
     }
   ]
 
   return (
     <Tab.Navigator >
       {
-        tabComponents.map(({ name, component },index) => {
+        tabComponents.map(({ name, component, focusedIcon, unFocusedIcon },index) => {
           return (
-            <Tab.Screen key={name} name={name} component={component} options={{ headerShown: false}} />
+            <Tab.Screen key={name} name={name} component={component} options={{ headerShown: false, tabBarIcon: ({ focused }) => (focused ? unFocusedIcon : focusedIcon) }} />
           )
         })
       }
