cd77862b78fd97615e702e8b2b4874d965fcb4fd
Brainless-Loco <dev.tcdas@gmail.com>
2024-11-30 00:27:49 +0600

    Minor Skeleton Update

--- screens/Dashboard/Dashboard.js
+++ screens/Dashboard/Dashboard.js
@@ -1,32 +1,23 @@
-import { View, Text } from 'react-native'
 import React, { useState } from 'react'
 import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Others from '../../components/Dashboard/Tabs/Others/Others'
-import Profile from '../../components/Dashboard/Tabs/Profile/Profile'
-
-// import { AntDesign, Ionicons, FontAwesome } from '@expo/vector-icons';
+import Home from '../../components/Dashboard/Tabs/Home/Home';
+import Activities from '../../components/Dashboard/Tabs/Activities/Activities';
+import AboutUs from '../../components/Dashboard/Tabs/AboutUs/AboutUs';
 
 const Tab = createBottomTabNavigator();
 
 export default function Dashboard({ route, navigation }) {
 
-  // const focusedIcons=[
-  //   <FontAwesome name="user-o" size={24} color="black" />,
-  //   <AntDesign name="infocirlceo" size={24} color="black" />
-  // ]
-
-  // const unFocusedIcons = [
-  //   <FontAwesome name="user" size={24} color="#0044ba" />,
-  //   <AntDesign name="infocirlce" size={24} color="#0044ba" />
-  // ]
-
   const tabComponents = [
     {
-      name: "Profile", component: Profile
+      name: "Home", component: Home
     },
     {
-      name: "Others", component: Others
+      name: "Activities", component: Activities
+    },
+    {
+      name: "About Us", component: AboutUs
     }
   ]
 
