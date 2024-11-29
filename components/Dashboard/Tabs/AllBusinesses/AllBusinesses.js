94815b9a6a3b25aacbfedd70d143c33d8e968598
Shoaib <shoaibu.ramim@gmail.com>
2024-11-30 03:28:31 +0600

    Updates on Design

--- components/Dashboard/Tabs/AllBusinesses/AllBusinesses.js
+++ components/Dashboard/Tabs/AllBusinesses/AllBusinesses.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function AboutUs() {
+export default function AllBusinesses() {
   return (
     <View>
-      <Text>AboutUs</Text>
+      <Text>All Businesses</Text>
     </View>
   )
 }
